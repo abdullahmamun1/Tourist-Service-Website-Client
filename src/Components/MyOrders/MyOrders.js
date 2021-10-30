71b72765f18ec792a6506972f84174ddf5d804d0
Abdullah Mamun <hello.abdullahmamun1@gmail.com>
2021-10-30 20:44:34 +0600

    my orders added

--- src/Components/MyOrders/MyOrders.js
+++ src/Components/MyOrders/MyOrders.js
@@ -10,7 +10,7 @@ const MyOrders = () => {
 
 
     useEffect(() => {
-        const url = `http://localhost:5000/orders/${userId}`
+        const url = `https://secret-meadow-63118.herokuapp.com/orders/${userId}`
         fetch(url)
             .then(res => res.json())
             .then(data => {
